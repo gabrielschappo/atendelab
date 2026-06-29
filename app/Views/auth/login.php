429b9373ff1b138120df8276e1984cacc0fecb77
Gabriel Schappo <schappogabriel0@gmail.com>
2026-06-29 19:43:59 -0300

    Ajuste erro mensagem login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -27,9 +27,9 @@ require_once __DIR__ . '/../layouts/config-view.php';
                     </div>
                 <?php endif; ?>
 
-                <?php if (!empty($erroLogin)): ?>
+                <?php if (!empty($erro)): ?>
                     <div class="alert alert-danger">
-                        <?= htmlspecialchars((string) $erroLogin, ENT_QUOTES, 'UTF-8') ?>
+                        <?= htmlspecialchars((string) $erro, ENT_QUOTES, 'UTF-8') ?>
                     </div>
                 <?php endif; ?>
 
