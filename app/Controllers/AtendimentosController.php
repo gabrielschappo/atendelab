ba3af97411bc886830ef0b80047429c5de87e8da
Gabriel Schappo <schappogabriel0@gmail.com>
2026-06-30 21:11:56 -0300

    Ajustes email não aparece e atendimento não salva

--- app/Controllers/AtendimentosController.php
+++ app/Controllers/AtendimentosController.php
@@ -58,7 +58,7 @@ class AtendimentosController
     {
         $pessoaId = filter_var($_POST['pessoa_id'] ?? null, FILTER_VALIDATE_INT);
         $tipoId = filter_var($_POST['tipo_atendimento_id'] ?? null, FILTER_VALIDATE_INT);
-        $usuarioId = filter_var($_POST['usuario_id'] ?? null, FILTER_VALIDATE_INT);
+        $usuarioId = $_SESSION['usuario']['id'] ?? null;
         $descricao = trim($_POST['descricao'] ?? '');
         $data = $_POST['data_atendimento'] ?? date('Y-m-d');
         $horario = $_POST['horario_atendimento'] ?? date('H:i');
